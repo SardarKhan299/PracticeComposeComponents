1a77079181460d2bfbf46b1a90f774d93470d1ae
sardarkhan <sardar@sideline.agency>
2024-11-27 00:33:31 +0500

    Add new TextField Class and practice its properties.

---
+++ app/src/main/java/com/example/samplecomposeapp/textField/TextFieldCompose.kt
@@ -0,0 +1,85 @@
+package com.example.samplecomposeapp.textField
+
+import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.samplecomposeapp.ui.theme.SampleComposeAppTheme
+
+
+@Composable
+fun TextFieldCompose(){
+
+    Column(modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        var text by remember { mutableStateOf("Type Here...") }
+        TextField(value = text,
+            onValueChange = { newText ->
+                text = newText
+            },
+            readOnly = false,
+            label = {
+                Text(text = "Enter Your Name")
+            },
+            singleLine = false,
+            maxLines = 2,
+            leadingIcon = {
+                IconButton(onClick = {}) {
+                    Icon(imageVector = Icons.Default.Email, contentDescription = "Email Icon")
+                }
+            },
+            trailingIcon = {
+                IconButton(onClick = {}) {
+                    Icon(imageVector = Icons.Default.Done, contentDescription = "Done Icon")
+                }
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Search
+            ),
+            keyboardActions = KeyboardActions(
+                onSearch = {
+                    Log.d("TAG", "TextFieldCompose: $text")
+                },
+                onDone = {
+                    Log.d("TAG", "TextFieldCompose: $text")
+                }
+            )
+
+        )
+
+
+    }
+}
+
+
+
+
+@Composable
+@Preview
+fun TextFieldPreview(){
+    SampleComposeAppTheme {
+        TextFieldCompose()
+    }
+}
