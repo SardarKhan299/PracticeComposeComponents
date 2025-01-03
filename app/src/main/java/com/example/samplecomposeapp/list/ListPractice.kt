1d03d5ae1bab65e14f5137e7174a45b4595b97c8
sardarkhan <sardar@sideline.agency>
2025-01-03 17:45:15 +0500

    check for recomposition

--- app/src/main/java/com/example/samplecomposeapp/list/ListPractice.kt
+++ app/src/main/java/com/example/samplecomposeapp/list/ListPractice.kt
@@ -1,16 +1,28 @@
 package com.example.samplecomposeapp.list
 
+import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.samplecomposeapp.textField.TextFieldCompose
 import com.example.samplecomposeapp.ui.theme.SampleComposeAppTheme
 
@@ -31,12 +43,36 @@ fun ListPractice(){
 
 }
 
+@Composable
+fun StatePractice(){
+  Log.d("ListPractice", ": State Practice ");
+  Column(
+    modifier = Modifier.fillMaxSize(),
+    verticalArrangement = Arrangement.Center,
+    horizontalAlignment = Alignment.CenterHorizontally
+  ) {
+    var count by remember { mutableIntStateOf(0) }
+    Log.d("ListPractice", ": Text Recomposing ");
+    Text(text = count.toString(),
+      fontSize = 30.sp)
+    Button(onClick = {
+      Log.d("ListPractice", ": Button Click.. ");
+      count++
+    }) {
+      Log.d("ListPractice", ":Button Text recomposition ");
+      Text(text = "Click Me $count")
+    }
+
+  }
+}
+
 
 
 @Preview(showBackground = true)
 @Composable
 fun ListPracticePreview(){
   SampleComposeAppTheme {
-    ListPractice()
+    //ListPractice()
+    StatePractice()
   }
 }
