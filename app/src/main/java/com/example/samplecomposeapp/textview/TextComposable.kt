51181f420048cbc8058b5aa695de4837f0c98dbe
sardarkhan <sardar@sideline.agency>
2025-01-03 11:50:43 +0500

    add box layout to textview

--- app/src/main/java/com/example/samplecomposeapp/textview/TextComposable.kt
+++ app/src/main/java/com/example/samplecomposeapp/textview/TextComposable.kt
@@ -1,14 +1,17 @@
 package com.example.samplecomposeapp.textview
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.selection.DisableSelection
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -87,16 +90,22 @@ fun GreetingText(name: String){
 
 @Composable
 fun TextOnEachOther(name: String){
-    Text(
-        text = "Hello. $name!",
-        color = Color.Blue,
-        fontSize = 30.sp,
-    )
-    Text(
-        text = "Some Other text",
-        color = Color.Blue,
-        fontSize = 30.sp,
-    )
+    Box(
+        modifier = Modifier
+            .size(400.dp)
+    ) {
+        Text(
+            text = "Hello. $name!",
+            color = Color.Blue,
+            fontSize = 30.sp,
+            modifier = Modifier.align(Alignment.BottomEnd)
+        )
+        Text(
+            text = "Some Other text",
+            color = Color.Blue,
+            fontSize = 30.sp,
+        )
+    }
 }
 
 @Preview(showBackground = true)
