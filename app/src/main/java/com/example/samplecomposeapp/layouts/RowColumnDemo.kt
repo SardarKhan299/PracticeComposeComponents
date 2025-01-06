6dfcd9bb60e90cadcbc6ece9bddb25af893da03d
sardarkhan <sardar@sideline.agency>
2025-01-06 10:45:39 +0500

    Add code to handle row with text baseline.

--- app/src/main/java/com/example/samplecomposeapp/layouts/RowColumnDemo.kt
+++ app/src/main/java/com/example/samplecomposeapp/layouts/RowColumnDemo.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.material3.VerticalDivider
@@ -15,8 +16,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.LastBaseline
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.samplecomposeapp.ui.theme.SampleComposeAppTheme
 
 @Composable
@@ -28,23 +31,48 @@ fun RowColumnDemo(modifier: Modifier = Modifier) {
 //    Text("Item 2", color = Color.White)
 //  }
 
+  // Row practice...//
+//  Row (
+//    modifier = Modifier.fillMaxSize(),
+//    verticalAlignment = Alignment.CenterVertically,
+//    horizontalArrangement = Arrangement.spacedBy(
+//      space = 20.dp, alignment = Alignment.CenterHorizontally)
+//    ){
+//    Box(
+//      modifier = Modifier.size(100.dp)
+//        .background(Color.Red)
+//    )
+//    Box(
+//      modifier = Modifier.size(100.dp)
+//        .background(Color.Blue)
+//        .align(Alignment.Bottom)
+//    )
+//    Box(
+//      modifier = Modifier.size(100.dp)
+//        .background(Color.Green)
+//    )
+//  }
+
   Row (
     modifier = Modifier.fillMaxSize(),
     verticalAlignment = Alignment.CenterVertically,
-    horizontalArrangement = Arrangement.SpaceEvenly
-    ){
-    Box(
-      modifier = Modifier.size(100.dp)
-        .background(Color.Red)
+    horizontalArrangement = Arrangement.spacedBy(
+      space = 20.dp, alignment = Alignment.CenterHorizontally)
+  ){
+    Text(
+      text = "Hello World",
+      fontSize = 40.sp,
+      color = Color.Blue,
+      modifier = Modifier
+        .width(120.dp)
+        .alignBy(LastBaseline)
     )
-    Box(
-      modifier = Modifier.size(100.dp)
-        .background(Color.Blue)
-    )
-    Box(
-      modifier = Modifier.size(100.dp)
-        .background(Color.Green)
-    )
+    Text(text = "Hello World 1..",
+      fontSize = 20.sp,
+      color = Color.Red,
+      modifier = Modifier.alignByBaseline()
+
+      )
   }
 }
 
