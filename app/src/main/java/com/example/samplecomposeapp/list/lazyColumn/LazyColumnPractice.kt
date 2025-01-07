e27f261452715f588551b8bfdd1efd86a11817d8
sardarkhan <sardar@sideline.agency>
2025-01-07 17:18:45 +0500

    Add Lazy Columns for Practice

---
+++ app/src/main/java/com/example/samplecomposeapp/list/lazyColumn/LazyColumnPractice.kt
@@ -0,0 +1,41 @@
+package com.example.samplecomposeapp.list.lazyColumn
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.samplecomposeapp.ui.theme.SampleComposeAppTheme
+
+
+@Composable
+fun LazyColumnPractice(modifier: Modifier = Modifier) {
+  LazyColumn (
+    modifier = Modifier.fillMaxSize()
+  ){
+    items(100){ i->
+      Text("item $i")
+    }
+    // last Item in List..//
+    item {
+      Text(
+        text = "Footer Reached the End...",
+        modifier = Modifier.fillMaxSize()
+          .background(Color.Red)
+      )
+    }
+  }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun LazyColumnPracticePreview() {
+  SampleComposeAppTheme {
+    LazyColumnPractice()
+  }
+}
