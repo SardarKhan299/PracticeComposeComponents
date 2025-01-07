4139b820f0040a22d41f4393fbcd7e4e16dafd3c
sardarkhan <sardar@sideline.agency>
2025-01-07 17:59:51 +0500

    Add Background color and padding.

--- app/src/main/java/com/example/samplecomposeapp/list/lazyColumn/LazyColumnPractice.kt
+++ app/src/main/java/com/example/samplecomposeapp/list/lazyColumn/LazyColumnPractice.kt
@@ -2,8 +2,12 @@ package com.example.samplecomposeapp.list.lazyColumn
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
@@ -11,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.samplecomposeapp.ui.theme.SampleComposeAppTheme
 
 
@@ -19,6 +24,11 @@ import com.example.samplecomposeapp.ui.theme.SampleComposeAppTheme
 fun LazyColumnPractice(modifier: Modifier = Modifier) {
   LazyColumn (
     modifier = Modifier.fillMaxSize()
+      .height(200.dp)
+      .padding(16.dp)
+      .background(Color.Red),
+    contentPadding = PaddingValues(16.dp),
+    verticalArrangement = Arrangement.SpaceEvenly
   ){
     items(100){ i->
       Text("item $i")
