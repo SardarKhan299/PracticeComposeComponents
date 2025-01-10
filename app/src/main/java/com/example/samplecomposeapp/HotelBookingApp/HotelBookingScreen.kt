1360a00fb18ab54043dbfe8f9b4ddabecabec8e9
sardarkhan <sardar@sideline.agency>
2025-01-10 18:03:02 +0500

    add assets for Hotel Booking Screen and create a new screen

---
+++ app/src/main/java/com/example/samplecomposeapp/HotelBookingApp/HotelBookingScreen.kt
@@ -0,0 +1,20 @@
+package com.example.samplecomposeapp.HotelBookingApp
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.samplecomposeapp.ui.theme.SampleComposeAppTheme
+
+@Composable
+fun HotelBookingScreen(modifier: Modifier = Modifier) {
+  
+}
+
+
+@Preview
+@Composable
+private fun HotelBookingScreenPreview() {
+  SampleComposeAppTheme {
+    HotelBookingScreen()
+  }
+}
