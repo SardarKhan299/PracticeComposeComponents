860d93fe120cb1e7380920092bf274c16d82c72e
sardarkhan <sardar@sideline.agency>
2025-01-10 12:06:24 +0500

    Use scaffold padding and also use snack bar.

--- app/src/main/java/com/example/samplecomposeapp/layouts/Scaffold.kt
+++ app/src/main/java/com/example/samplecomposeapp/layouts/Scaffold.kt
@@ -1,5 +1,10 @@
 package com.example.samplecomposeapp.layouts
 
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Add
@@ -12,18 +17,31 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.samplecomposeapp.ui.theme.SampleComposeAppTheme
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ScaffoldPractice() {
 
+  val snackBarState = remember {
+    SnackbarHostState()
+  }
+
+  val scope = rememberCoroutineScope()
   Scaffold(
     topBar = {
       TopAppBar(
@@ -70,13 +88,34 @@ fun ScaffoldPractice() {
     }
 },
     floatingActionButton = {
-      FloatingActionButton(onClick = {}) {
+      FloatingActionButton(onClick = {
+        Log.d("Scaffold", "Clicked..: ");
+        scope.launch {
+          snackBarState.showSnackbar("FAB Clicked...")
+        }
+
+      }) {
         Icon(imageVector = Icons.Default.Add,
           contentDescription = null)
       }
+    },
+    snackbarHost = {
+      SnackbarHost(
+        hostState = snackBarState,
+      ) {  }
     }
   ) {
-
+    contentPadding ->
+      Box(
+        modifier = Modifier
+          .fillMaxSize()
+          .padding(contentPadding)
+          .background(Color.Red)
+      ){
+        Text(text = "Hello Text Box...",
+          fontSize = 16.sp
+          )
+      }
   }
 }
 
