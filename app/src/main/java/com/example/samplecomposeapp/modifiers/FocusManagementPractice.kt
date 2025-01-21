7eb0ccc3e78ac610aa7cb5d85a3b46482560937f
sardarkhan <sardar@sideline.agency>
2025-01-21 13:05:27 +0500

    Make Box Composable focusable. and change the color when focused

--- app/src/main/java/com/example/samplecomposeapp/modifiers/FocusManagementPractice.kt
+++ app/src/main/java/com/example/samplecomposeapp/modifiers/FocusManagementPractice.kt
@@ -1,9 +1,14 @@
 package com.example.samplecomposeapp.modifiers
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -11,12 +16,16 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.input.ImeAction
@@ -70,17 +79,33 @@ fun FocusManagementPractice(modifier: Modifier = Modifier) {
         imeAction = ImeAction.Next
       )
     )
+    var isFocused by remember {
+      mutableStateOf(false)
+    }
+    // Box are not by default focusable..//
+    Box(
+      modifier = Modifier.fillMaxWidth()
+        .height(100.dp)
+        .border(
+          width = 4.dp,
+          color = if(isFocused) Color.Red else Color.Green
+        )
+        .onFocusChanged {
+          isFocused = it.isFocused
+        }
+        .focusable()
+    )
     TextField(
       value = "",
       onValueChange = {
 
       }
     )
+
     Button(onClick = {
       focusRequester.requestFocus()
     }) {
       Text(text = "Start Filling The Form")
-
     }
   }
 
